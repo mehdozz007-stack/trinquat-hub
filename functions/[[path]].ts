a3628940aba8b34390618656a285e49f61c0132f
Mehdi Zitouni <mehdi.zitouni1@gmail.com>
2026-07-03 01:05:13 +0200

    feat: add maintenance page 'Site en cours de construction'

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -22,6 +22,20 @@ export async function onRequest(context) {
     });
   }
 
+  // Serve maintenance page for all other requests
+  if (url.pathname === '/' || url.pathname === '') {
+    try {
+      const maintenanceUrl = new URL('/maintenance.html', request.url);
+      const response = await fetch(maintenanceUrl.toString());
+      return new Response(response.body, {
+        status: 200,
+        headers: { 'Content-Type': 'text/html; charset=utf-8' },
+      });
+    } catch (e) {
+      return new Response('Site en cours de construction', { status: 200 });
+    }
+  }
+
   // Otherwise, proceed with normal Pages serving
   return context.next();
 }
